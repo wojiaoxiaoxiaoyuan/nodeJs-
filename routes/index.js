f16878e86dcbc80fa31a9adec98dc578144d805b
袁泽林 <yuan.zelin@aquilaflycloud.com>
2019-04-10 17:00:22 +0800

    完成框架目录结构

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,4 @@
-module.exports = function (app) {
-  //一个get请求的路由  http://localhost:3000
-  app.get("/", function (req, res) {
-      res.render("index", {title:"育知同创abc"})
-  });
-  
-  //又一个请求路由：http://localhost:3000/abc
-  app.get("/abc", function (req, res) {
-      res.render("index", {title:"育知同创" + req.path})
-  });
+module.exports = app => {
+  // 测试服接口Test
+  app.get('/test', (req, res) => { require('./test')(req, res) })
 }
---
+++ routes/test/index.js
@@ -0,0 +1,5 @@
+// http://localhost:8080/test
+
+module.exports = (req, res) => {
+  res.send('Hello, World')
+}
